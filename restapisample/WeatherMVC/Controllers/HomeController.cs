03afa437fd32f75b69b170f6178cb40098a5abf9
MatiasDevop <solomatias321@gmail.com>
2021-02-16 15:25:22 +0100

    implemting mvc to test our ids4 and gettoken service

---
+++ restapisample/WeatherMVC/Controllers/HomeController.cs
@@ -0,0 +1,72 @@
+﻿using IdentityModel.Client;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using WeatherMVC.Models;
+using WeatherMVC.Services;
+
+namespace WeatherMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public readonly ITokenService _tokenService;
+
+        public HomeController(ITokenService tokenService, ILogger<HomeController> logger)
+        {
+            _tokenService = tokenService;
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+        public async Task<IActionResult> Weather()
+        {
+            var data = new List<WeatherData>();
+            using (var client = new HttpClient())
+            {
+                var tokenResponse = await _tokenService.GetToken("weatherapi.read");
+
+                client
+                  .SetBearerToken(tokenResponse.AccessToken);
+
+                var result = client
+                  .GetAsync("https://localhost:5445/weatherforecast")
+                  .Result;
+
+                if (result.IsSuccessStatusCode)
+                {
+                    var model = result.Content.ReadAsStringAsync().Result;
+
+                    data = JsonConvert.DeserializeObject<List<WeatherData>>(model);
+
+                    return View(data);
+                }
+                else
+                {
+                    throw new Exception("Unable to get content");
+                }
+            }
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
