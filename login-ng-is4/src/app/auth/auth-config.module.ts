897c4ee7d49b4931354fa54824c0252b0b3a83de
MatiasDevop <solomatias321@gmail.com>
2021-02-18 12:24:26 +0100

    fixing authentication client angular to identity server

--- login-ng-is4/src/app/auth/auth-config.module.ts
+++ login-ng-is4/src/app/auth/auth-config.module.ts
@@ -12,29 +12,5 @@ export function configureAuth(oidcConfigService: OidcConfigService): () => Promi
             scope: 'openid profile weatherapi.read',
             silentRenew: true,
             logLevel: LogLevel.Debug,
-            //     stsServer: 'https://localhost:5443',//'my-super-sts',
-        //     redirectUrl: window.location.origin,
-        //     postLogoutRedirectUri: window.location.origin,
-        //     clientId: 'interactive', //'please-enter-clientId',
-        //     scope: 'openid profile weatherapi.read', // 'openid profile ' + your scopes
-        //     responseType: 'code',
-        //     silentRenew: true,
-        //    // silentRenewUrl: window.location.origin + '/silent-renew.html',
-        //     renewTimeBeforeTokenExpiresInSeconds: 10,
         });
 }
-
-// @NgModule({
-//     imports: [AuthModule.forRoot()],
-//     exports: [AuthModule],
-//     providers: [
-//         OidcConfigService,
-//         {
-//             provide: APP_INITIALIZER,
-//             useFactory: configureAuth,
-//             deps: [OidcConfigService],
-//             multi: true,
-//         },
-//     ],
-// })
-// export class AuthConfigModule {}
